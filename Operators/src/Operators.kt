161d859d67366a4fb63488f466d2b446b44c2157
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-01-29 10:56:18 -0600

    Add files via upload
    Practicing with operators in kotlin

---
+++ Operators/src/Operators.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var num = 5
+    var add = num + 7
+    var sub = num - 3
+    var mult = num * 12
+    var div = num/2
+    var remain = num % 2
+
+    println("$num + 7 = $add")
+    println("$num - 3 = $sub")
+    println("$num * 12 = $mult")
+    println("$num / 2 = $div")
+    println("$num % 2 = $remain")
+    
+}
