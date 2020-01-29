87d9c7f53f81e105e4a72d4caf2fad45933a182c
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-01-29 10:36:43 -0600

    Add files via upload
    Ver control/about me program

---
+++ VerControl/src/AboutMe.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("About me")
+    println("Name Jacob Miller")
+    println("Age: 26")
+    println("Hobbies: Running, Martial Arts, Programming")
+    println("Works at: Amazon as delivery driver")
+}
