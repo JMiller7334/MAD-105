a08d702e8c516bc01ace82337c304dea081991cd
JMiller7334 <42582030+JMiller7334@users.noreply.github.com>
2020-01-29 10:48:06 -0600

    Add files via upload
    Practicing Variables

---
+++ Variable Practice.kt
@@ -0,0 +1,40 @@
+
+fun main() {
+
+    var demoByte = 1
+    var demoShort = 2
+    var demoInt = 10
+    var demoLong = 12345678
+    var demoFloat = 1.553
+    var demoDouble = 10.556
+    var demoBool = false
+    var demoChar = "f"
+
+    print("byte ")
+    println(demoByte)
+
+    print("short ")
+    println(demoShort)
+
+    print("int ")
+    println(demoInt)
+
+    print("Long ")
+    println(demoLong)
+
+    print("float ")
+    println(demoFloat)
+
+    print("Double ")
+    println(demoDouble)
+
+    print("Bool ")
+    println(demoBool)
+
+    print("Char ")
+    println(demoChar)
+
+}
+
+
+
